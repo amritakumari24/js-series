b7c18244c6473331fbd686b1b58cc7ef9b124fc4
Amrita <amritakumari24@navgurukul.org>
2026-07-15 18:28:56 +0530

    milestone2

---
+++ milestone2/mile2.js
@@ -0,0 +1,45 @@
+// console.log(10 + "5" - 2 * 3);
+// console.log("10" + 5 - "2" + 1);
+
+// console.log(true + true);
+// console.log(false + 10);
+// console.log(true + "10");
+
+
+// let a = 5;
+
+// console.log(a++);
+// console.log(++a);
+// console.log(a);
+
+// let num = 18;
+
+// if(num / 2 === parseInt(num / 2)){
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd");
+// }
+
+
+// console.log(5 == "5");
+// console.log(5 === "5");
+// console.log(null == undefined);
+// console.log(null === undefined);
+
+// let age = 20;
+// let citizen = false;
+
+// if(age >= 18 && citizen || age > 60){
+//     console.log("Eligible");
+// }
+// else{
+//     console.log("Not Eligible");
+// }
+
+// && and || can return other values as well rather than only returning true and false.
+
+// console.log(10&&20) it stops when value is false and then return and if both values are true then return last one
+
+// console.log(0||20) it return true value always 
+
