3313d234502c1e3a806572a488d081a8f633cfcf
Amrita <amritakumari24@navgurukul.org>
2026-07-16 12:45:32 +0530

    milestone2-Closures

--- milestone2/mile2.js
+++ milestone2/mile2.js
@@ -5,7 +5,6 @@
 // console.log(false + 10);
 // console.log(true + "10");
 
-
 // let a = 5;
 
 // console.log(a++);
@@ -20,7 +19,6 @@
 // else{
 //     console.log("Odd");
 // }
-
 
 // console.log(5 == "5");
 // console.log(5 === "5");
@@ -41,5 +39,166 @@
 
 // console.log(10&&20) it stops when value is false and then return and if both values are true then return last one
 
-// console.log(0||20) it return true value always 
+// console.log(0||20) it return true value always
 
+// var x = 10;
+
+// function test(){
+//     console.log(x);
+//     var x = 20;
+// }
+
+// test();
+
+// var x = 1;
+
+// function outer(){
+
+//     var x = 2;
+
+//     function inner(){
+//         console.log(x);
+//     }
+
+//     inner();
+// }
+
+// outer();
+
+// console.log("Start");
+
+// function first(){
+//     console.log("First");
+//     second();
+// }
+
+// function second(){
+//     console.log("Second");
+// }
+
+// first();
+
+// console.log("End");
+
+// function outer(){
+
+//     let name = "Rahul";
+
+//     function inner(){
+//         console.log(name);
+//     }
+
+//     inner();
+// }
+
+// outer();
+
+// closure = A closure is a JavaScript feature where an inner function remembers and maintains access to the variables of its outer lexical scope, even after the outer function has finished its execution.
+
+//How to identify Closure:-
+// 1.)inner function reference to the outer function by lexical scope 
+// return or wait for run the outer function means no immediate call to inner function.
+
+
+
+// function outer(){
+
+//     let count = 0;
+
+//     return function inner(){
+//         count++;
+//         console.log(count);
+//     }
+// }
+
+// let counter = outer();
+
+// counter();
+// counter();
+
+// function outer(){
+
+//     let x = 10;
+
+//     return function inner(){
+//         console.log(x);
+//     }
+// }
+
+// let fn = outer();
+
+// let x = 20;
+
+// fn();
+
+
+// for(let i = 1; i <= 3; i++){
+
+//     setTimeout(function(){
+//         console.log(i);
+//     },1000);
+
+// }
+
+// function create(){
+
+//     let x = 10;
+
+//     return function(){
+//         console.log(x);
+//     }
+// }
+
+// let a = create();
+// let b = create();
+
+// a();
+// b();
+
+// function create(){
+
+//     let x = 10;
+
+//     return function(){
+//         x++;
+//         console.log(x);
+//     }
+// }
+
+// let a = create();
+
+// a();
+// a();
+
+// let b = create();
+
+// b();
+
+// function outer(){
+
+//     let x = 1;
+
+//     return {
+//         first: function(){
+//             x++;
+//             console.log(x);
+//         },
+
+//         second: function(){
+//             x++;
+//             console.log(x);
+//         }
+//     }
+// }
+
+// let obj = outer();
+
+// obj.first();
+
+// obj.second();
+
+// obj.first();
+
+// Closure Master Rule:
+// Same outer function call → inner functions same closure share kar sakte hain
+// Different outer function calls → separate closure environments bante hain
