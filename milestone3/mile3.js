7ee79d410b7107992cbe223ab6ac9446207ed6e2
Amrita <amritakumari24@navgurukul.org>
2026-07-18 17:26:31 +0530

    initiated milstone3

---
+++ milestone3/mile3.js
@@ -0,0 +1,72 @@
+// let name = "Rahul";
+
+// function test(){
+
+// console.log(name);
+
+// }
+
+// console.log(name);
+
+// function findFactorial(n){
+//     let fact =1 
+//     for (let i=1; i <=n;i++){
+//         fact *= i
+//     }
+//     console.log(fact)
+
+// }
+// findFactorial(5);
+
+
+// function fibonacci(n){
+//    let a = 0;
+//    let b = 1;
+//    for(let i=0; i<n; i++){
+    
+//     let c = a+b;
+//       a=b;
+//       b=c;
+//    }
+//    console.log(a)
+// }
+
+// fibonacci(5)
+
+// let a=0,b=1;
+
+// for(let i=1;i<=10;i++){
+
+
+
+// let c=a+b;
+
+// a=b;
+
+// b=c;
+
+// }
+// console.log(a);
+
+
+// sayHi();
+
+// const sayHi = function () {
+//     console.log("Hello");
+// };
+
+
+// greet();
+
+// function greet() {
+//     console.log("Hello");
+// }
+
+
+
+// function test() {
+
+//     console.log("Hello");
+// }
+
+// console.log(test());
