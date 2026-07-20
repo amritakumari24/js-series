e4b05e4dd654aad8dd512fc1da25a8661bfaa70a
Amrita <amritakumari24@navgurukul.org>
2026-07-20 22:36:59 +0530

    initiated milestone4

---
+++ milestone4/mile4.js
@@ -0,0 +1,28 @@
+// let arr = [10,20,30,40];
+
+// console.log(arr.slice(1,3));
+// console.log(arr)
+
+// let arr = [10,20,30,40];
+
+// console.log(arr.splice(1,2));
+ 
+// console.log(arr);
+
+// let arr = ['amrita', 'aliya', 'dolly']
+// console.log(arr.includes('amrita'))
+
+// let = [10,20,30]
+// console.log(let.indexOf(20))
+
+
+// let = "amrita";
+// console.log(let.length)
+
+
+// let text = "  Hello  ";
+
+// console.log(text.trim());
+
+let text = "A,B,C";
+console.log(text.split(","))
