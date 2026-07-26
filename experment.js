7a1dbcbfd7293dd31da92ed51195a65ec7bd1afc
Amrita <amritakumari24@navgurukul.org>
2026-07-26 14:30:36 +0530

    object question

--- experment.js
+++ experment.js
@@ -8,3 +8,35 @@
 // type corecsion
 
 // quirks 
+
+// Sort Students by Marks 
+// Problem
+// Sort students in ascending order of marks.
+// Input
+const students = [
+  { name: "A", marks: 90 },
+  { name: "B", marks: 70 },
+  { name: "C", marks: 85 }
+];
+// Output
+// [
+//   { name: "B", marks: 70 },
+//   { name: "C", marks: 85 },
+//   { name: "A", marks: 90 }
+// ]
+// Concepts
+// Sorting
+// Objects
+
+
+for (let i=0; i<=students.length-1; i++){
+    for(let j=0; j<students.length-i-1; j++){
+        if(students[j].marks > students[j+1].marks){
+            [students[j], students[j+1]]=[ students[j+1], students[j]]
+
+        }
+    }
+}
+console.log(students)
+
+
