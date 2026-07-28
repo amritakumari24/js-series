ddbbc658dadd5848ec7604a8204e86c5c53f31c5
Amrita <amritakumari24@navgurukul.org>
2026-07-28 15:44:11 +0530

    mile4

--- milestone4/mile4.js
+++ milestone4/mile4.js
@@ -26,3 +26,56 @@
 
 // let text = "A,B,C";
 // console.log(text.split(","))
+
+
+// const arr = [1, 2, 3];
+// const result = arr.push(4);
+
+// console.log(result);
+
+// let arr = [1,2,3];
+// arr.splice(1,1,10,20);
+
+// console.log(arr);
+
+// let arr = [1,2,3];
+// arr.splice(1,1,10,20);
+
+// console.log(arr);
+// let arr = ["A","B","C"];
+
+// console.log(arr.indexOf("D"));
+
+
+// const str = " JavaScript ";
+
+// console.log(str.trim().length);
+
+// const obj = {
+//     a:1,
+//     b:2
+// };
+
+// delete obj.a;
+
+// console.log(obj);
+
+// const user = {
+//     profile:{
+//         name:"Aman"
+//     }
+// };
+
+// console.log(user.profile.name);
+
+12
+
+
+// let arr = ["A","B","C"];
+
+// let x = arr.slice(1);
+
+// arr.splice(0,1);
+
+// console.log(x);
+// console.log(arr);
