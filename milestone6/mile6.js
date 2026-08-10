46c8685a805b67ed4a48bb0bee65a0cdcc9d2377
Amrita <amritakumari24@navgurukul.org>
2026-08-10 16:40:16 +0530

    domTask1

--- milestone6/mile6.js
+++ milestone6/mile6.js
@@ -1,1 +1,20 @@
-//dom 
+const count = document.getElementById("count");
+const increase = document.getElementById("increament");
+const decrease = document.getElementById("decreament");
+
+let value = 0;
+
+increase.addEventListener("click", function(){
+value++
+count.innerText = value;
+});
+
+decrease.addEventListener("click", function(){
+    value--
+    count.innerText = value;
+
+})
+
+reset.addEventListener("click", function(){
+    count.innerText = 0;
+})
