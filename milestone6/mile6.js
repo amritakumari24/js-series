25e0e368407ed660419f2d3413130d1fbd284768
Amrita <amritakumari24@navgurukul.org>
2026-08-11 18:21:19 +0530

    task2

--- milestone6/mile6.js
+++ milestone6/mile6.js
@@ -1,6 +1,8 @@
 const count = document.getElementById("count");
 const increase = document.getElementById("increament");
 const decrease = document.getElementById("decreament");
+const themeBtn = document.getElementById("themeBtn");
+
 
 let value = 0;
 
@@ -17,4 +19,16 @@ decrease.addEventListener("click", function(){
 
 reset.addEventListener("click", function(){
     count.innerText = 0;
+})
+
+themeBtn.addEventListener("click", function(){
+    if (document.body.style.background === "white"||
+         document.body.style.background === ""
+    ){
+       document.body.style.background = "black";
+       document.body.style.color = "white";
+    }else{
+         document.body.style.background = "white";
+         document.body.style.color = "black";
+    }
 })
