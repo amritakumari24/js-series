c4a0f35f9f341faf5261bc5991fa5861eca22c59
Amrita <amritakumari24@navgurukul.org>
2026-08-25 22:37:46 +0530

    event loop theory with example

--- milestone9/mile9.js
+++ milestone9/mile9.js
@@ -1,2 +1,12 @@
-console.log(this)
-console.log(this === module.exports);
+// console.log(this)
+// console.log(this === module.exports);
+
+// revised event loop with diagram 
+// micro queue 
+//macro queue
+// micro queue starvation 
+// broswer api
+// fetch() [network  ]
+// dom api  [mutation Observer]
+// setTimeout() [timer]
+
