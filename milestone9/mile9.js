6655915ef9340e520b9d60ab7922327217e84e91
Amrita <amritakumari24@navgurukul.org>
2026-08-29 22:55:02 +0530

    HOF

--- milestone9/mile9.js
+++ milestone9/mile9.js
@@ -27,3 +27,114 @@
 // var b = 10; 
 // a()//10
 
+//high order function
+
+// function greet(name) {
+//     return "Hello " + name;
+// }
+
+// function process(fn) {
+//     return fn("Rahul");
+// }
+
+// console.log(process(greet));
+
+// function calculate(a, b, fn) {
+//     return fn(a, b);
+// }
+
+// function add(x, y) {
+//     return x + y;
+// }
+
+// console.log(calculate(10, 5, add));
+
+// function operation(a, b, fn) {
+//     return fn(a, b);
+// }
+
+// console.log(operation(6, 3, function(x, y) {
+//     return x - y;
+// }));
+
+// function multiplyByTwo(num) {
+//     return num * 2;
+// }
+
+// function process(arr, fn) {
+//     return arr.map(fn);
+// }
+
+// console.log(process([1, 2, 3], multiplyByTwo));
+
+// function test(fn) {
+//     return fn(5);
+// }
+
+// console.log(test(function(x) {
+//     return x * 2;
+// }) + 10);
+
+// function outer(fn) {
+//     return fn(10);
+// }
+
+// function inner(x) {
+//     return x + 5;
+// }
+
+// console.log(outer(inner));
+
+
+// function calculate(num, fn) {
+//     return fn(num);
+// }
+
+// function double(x) {
+//     return x * 2;
+// }
+
+// function addFive(x) {
+//     return x + 5;
+// }
+
+// console.log(calculate(10, double));
+// console.log(calculate(10, addFive));
+
+// function process(num, fn) {
+//     return fn(num);
+// }
+
+// function square(x) {
+//     return x * x;
+// }
+
+// console.log(process(4, square) + process(3, square));
+
+// function calculate(a, b, fn) {
+//     return fn(a, b);
+// }
+
+// function add(x, y) {
+//     return x + y;
+// }
+
+// function multiply(x, y) {
+//     return x * y;
+// }
+
+// console.log(calculate(2, 3, add) * calculate(2, 3, multiply));
+
+function process(num, fn) {
+    return fn(num);
+}
+
+function double(x) {
+    return x * 2;
+}
+
+function addTen(x) {
+    return x + 10;
+}
+
+console.log(process(process(5, double), addTen))
