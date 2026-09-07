bd07d51f3a74e6ee420eec5bdca7ae02d5030b51
Amrita <amritakumari24@navgurukul.org>
2026-09-07 17:28:32 +0530

    call-apply-bind

--- milestone12/mile12.js
+++ milestone12/mile12.js
@@ -1,16 +1,26 @@
 let name = {
     firstName: 'Amrita',
     lastName: "Pandit",
-    printfullName : function(){
-        console.log(this.firstName+" "+ this.lastName)
-    }
+  
 }
-name.printfullName();
+
+ let  printfullName = function(hometown, state){
+        console.log(this.firstName+" "+ this.lastName , hometown, state)
+    }
+
 
 let name2 = {
     firstName: "devansh",
     lastName: "pandit"
 
 }
-// function borrowing 
-name.printfullName.call(name2)
+// function borrowing  ,,call()   // used to directly call the function
+printfullName.call(name, "baru", "hp");
+
+// apply() //same as call(),just takes array for multiple arguments
+printfullName.apply(name2, ["korba", "cg" ])
+
+// bind() // it gives the copy of function nstead of diretly call the function to use it later 
+let printMyName  = printfullName.bind(name2, "heloo" , "bye")
+console.log(printMyName)
+printMyName()
